e120985d6aad602c5607d0cce31d5cf6838c3f34
Markus Kleiton <102915789+MineStoreCMS@users.noreply.github.com>
2025-05-06 03:32:35 +0300

    Updated server API fetcher to transfer request IP to backend

--- src/api/server/fetcher.ts
+++ src/api/server/fetcher.ts
@@ -1,10 +1,23 @@
-import axios from "axios"
-import { cookies } from "next/headers"
-import { config } from "../config"
+import axios from 'axios';
+import { cookies, headers } from 'next/headers';
+import { config } from '../config';
 
-export const fetcher = axios.create(config)
+export const fetcher = axios.create(config);
 
 fetcher.interceptors.request.use((request) => {
-   request.headers.Authorization = `Bearer ${cookies().get("token")?.value}`
-   return request
-})
+   const headersList = headers();
+
+   const clientIP =
+      headersList.get('x-forwarded-for')?.split(',')[0].trim() || // Most common
+      headersList.get('x-real-ip') || // Nginx, etc.
+      headersList.get('http_x_real_ip') || // Apache, etc.
+      headersList.get('x-client-ip') || // Less common
+      headersList.get('x-forwarded') || // General header
+      headersList.get('forwarded-for') || // Another variation
+      headersList.get('x-vercel-proxied-for') || // Vercel-specific
+      '127.0.0.1'; // Fallback to localhost
+
+   request.headers.Authorization = `Bearer ${cookies().get('token')?.value}`;
+   request.headers['X-Forwarded-For'] = clientIP;
+   return request;
+});
