b6af3db48cb9593d9cf56ee68a5471cfa9f0fa54
Markus Kleiton <markus.kleiton.1998@gmail.com>
2025-05-07 02:28:16 +0300

    Homepage improvements for future integration

--- src/app/(pages)/home/components/payment-methods.tsx
+++ src/app/(pages)/home/components/payment-methods.tsx
@@ -1,56 +1,79 @@
 'use client';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
+import { useSettingsStore } from '@/stores/settings';
 
 export function PaymentMethods() {
-    const t = useTranslations('home');
+   const t = useTranslations('home');
+   const { settings } = useSettingsStore();
 
-    return (
-        <div className="mt-10">
+   const payNow = settings?.footer?.find(item => item.url === "https://paynow.gg/terms-of-use")?.url;
+
+   return (
+      <div className="mt-10">
+         {payNow && (
             <div className="flex-row py-4">
-                <div className="w-32">
-                    <Image
+               <div className="w-32">
+                  <Image
+                     className="h-[29px] w-28 object-contain"
+                     src={`/media/home/paynow.svg`}
+                     alt="PayNow Logo"
+                     width={112}
+                     height={29}
+                  />
+               </div>
+               <span className="ml-12">PayPal, Cards, Bank Transfers & 75+ More Ways to Pay.</span>
+            </div>
+         )}
+
+         {!payNow && (
+            <>
+               <div className="flex-row py-4">
+                  <div className="w-32">
+                     <Image
                         className="h-[29px] w-28 object-contain"
                         src={`/media/home/paypal.svg`}
                         alt="PayPal Logo"
                         width={112}
                         height={29}
-                    />
-                </div>
-                <span className="ml-12">{t('paypal-balance-banks-all-credit-debit-cards')}</span>
-            </div>
+                     />
+                  </div>
+                  <span className="ml-12">{t('paypal-balance-banks-all-credit-debit-cards')}.</span>
+               </div>
 
-            <hr className="w-64" />
+               <hr className="w-64" />
 
-            <div className="flex-row py-4">
-                <div className="w-32">
-                    <Image
+               <div className="flex-row py-4">
+                  <div className="w-32">
+                     <Image
                         className="mr-auto h-[40px] w-28 object-contain"
                         src={`/media/home/skrill.svg`}
                         alt="Skrill Logo"
                         width={112}
                         height={40}
-                    />
-                </div>
-                <span className="ml-12">
-                    {t('paysafecard-amazon-pay-mobile-payments-600-more')}
-                </span>
-            </div>
+                     />
+                  </div>
+                  <span className="ml-12">
+                      {t('paysafecard-amazon-pay-mobile-payments-600-more')}
+                   </span>
+               </div>
 
-            <hr className="w-64" />
+               <hr className="w-64" />
 
-            <div className="flex-row py-4">
-                <div className="w-32">
-                    <Image
+               <div className="flex-row py-4">
+                  <div className="w-32">
+                     <Image
                         className="h-[29px] w-28 object-contain"
                         src={`/media/home/stripe.svg`}
                         alt="Stripe Logo"
                         width={112}
                         height={29}
-                    />
-                </div>
-                <span className="ml-12">{t('all-credit-debit-cards')}</span>
-            </div>
-        </div>
-    );
+                     />
+                  </div>
+                  <span className="ml-12">{t('all-credit-debit-cards')}.</span>
+               </div>
+            </>
+         )}
+      </div>
+   );
 }
